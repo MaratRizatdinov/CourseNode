26068d167f7e1045f924af5d2b1c9b9b16c1d4e4
MaratRizatdinov <maratrizatdinov91@gmail.com>
2023-11-07 08:39:05 +0300

    readme

--- src/modules/users.js
+++ src/modules/users.js
@@ -3,7 +3,7 @@ const path = require("path");
 
 const getUsers = () => {
     const filePath = path.join(__dirname, "../data/users.json");
-    return fs.readFileSync(filePath);
+    return fs.readFileSync(filePath,{encoding:'utf8'});
 };
 
 module.exports = getUsers;
